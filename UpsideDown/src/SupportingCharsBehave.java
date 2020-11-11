df9dd66a368f6e7bdff1c92edbc0a8b18ab40265
Nadeen <g00079991@aus.edu>
2020-11-11 20:03:54 +0400

    First commit with the character beginning

---
+++ UpsideDown/src/SupportingCharsBehave.java
@@ -0,0 +1,4 @@
+
+public interface SupportingCharsBehave {
+	public void talk();
+}
--- UpsideDown/src/driver.java
+++ UpsideDown/src/driver.java
@@ -3,6 +3,11 @@ public class driver {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		Person p  = new PoliceOfficer("Polyy",40, 0); 
+		System.out.println(p);
+		Person v = Victim.getInstance("Visha",50, 0);
+		System.out.println(v);
+
 	}
 
 }
