f0fec9ecd26081cc371bc2e1bda728166b9b9148
NouranSheta <g00080065@aus.edu>
2020-11-13 18:08:12 +0400

    added objects

---
+++ UpsideDown/src/upsidedown/SecurityCamera (2).java
@@ -0,0 +1,51 @@
+package upsidedown;
+
+public class SecurityCamera extends Object{
+
+	public SecurityCamera() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public SecurityCamera(String name, boolean use, String color) {
+		super(name, use, color);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void checkLevel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void checkDescription() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void use() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void errorMessage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void collect() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void store() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
